d3559ac166cfb937d2580c7095668e040929431a
Otto Poikajärvi <tootsi666@gmail.com>
2019-08-13 17:04:08 +0300

    func defs

--- Rpi_BrowserCam/Rpi_BrowserCam.py
+++ Rpi_BrowserCam/Rpi_BrowserCam.py
@@ -1,1 +1,12 @@
 #This is the main file
+
+import subprocess
+
+def takemcastpic():
+    subprocess.run(["python3", "sendcmd_os.py", "-o", "newimage.jpg"])
+
+def startpiclisten():
+    subprocess.run(["python3", "listen_os.py"])
+
+def startstreamserver():
+    subprocess.run(["python3", "cameraserver.py"])
