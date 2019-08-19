f0ede4702270f6a71c5c32802001a4081161fce1
Otto Poikajärvi <tootsi666@gmail.com>
2019-08-19 16:22:13 +0300

    SD answers

--- Rpi_BrowserCam/servicediscovery_client.py
+++ Rpi_BrowserCam/servicediscovery_client.py
@@ -5,12 +5,25 @@ import socket
 MCAST_ADDR = "224.1.1.1"
 MCAST_PORT = 5008
 TOKEN = "rpiservice" #Has to be identical for both client and server
+TOKEN_ANS = "answer" #Has to be identical for both client and server
+MULTICAST_TTL = 2 #how many hops for packets
+devicesfound = []
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
 sock.sendto(TOKEN.encode("utf-8"), (MCAST_ADDR, MCAST_PORT))
-data, server = sock.recvfrom(10240)
-data = data.decode("utf-8")
-print(data)
-print(server)
-sock.close()
+
+while True:
+    sock.settimeout(1)
+    try:
+        data, server = sock.recvfrom(10240)
+        data = data.decode("utf-8")
+        #print(data)
+        #print(server)
+        if data.startswith(TOKEN_ANS):
+            devicesfound.append(server[0])
+    except socket.timeout:
+        print("No more new devices found")
+        print(devicesfound)
+        sock.close()
+        break
--- Rpi_BrowserCam/servicediscovery_server.py
+++ Rpi_BrowserCam/servicediscovery_server.py
@@ -6,25 +6,22 @@ import struct
 MCAST_ADDR = "224.1.1.1"
 MCAST_PORT = 5008
 TOKEN = "rpiservice" #Has to be identical for both client and server
+TOKEN_ANS = "answer" #Has to be identical for both client and server
 
-def get_rpi_ipaddr():
-    testsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    testsock.connect(("8.8.8.8", 80))
-    ipaddr = testsock.getsockname()[0]
-    testsock.close()
-    return ipaddr
+def listen_socket():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(("", MCAST_PORT))
+    mreq = struct.pack("4sl", socket.inet_aton(MCAST_ADDR), socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    return sock
 
-ipaddr = get_rpi_ipaddr()
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind((MCAST_ADDR, MCAST_PORT))
-mreq = struct.pack("4sl", socket.inet_aton(MCAST_ADDR), socket.INADDR_ANY)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+sock = listen_socket()
 
 while True:
     data, client = sock.recvfrom(10240)
-    data.decode("utf-8")
+    data = data.decode("utf-8")
     print(data)
     print(client)
     if data.startswith(TOKEN):
-        sock.sendto(ipaddr.encode("utf-8"), client)
+        sock.sendto(TOKEN_ANS.encode("utf-8"), client)
