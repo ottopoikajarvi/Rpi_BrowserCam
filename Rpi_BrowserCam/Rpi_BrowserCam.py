902584a823225992dbe0d55f87640a4406c5fa93
Otto Poikajärvi <tootsi666@gmail.com>
2019-08-27 14:12:55 +0300

    Start servers script

--- Rpi_BrowserCam/Rpi_BrowserCam.py
+++ Rpi_BrowserCam/Rpi_BrowserCam.py
@@ -5,8 +5,23 @@ import subprocess
 def takemcastpic():
     subprocess.Popen(["python3", "takeimagecmd.py"])
 
-def startpiclisten():
-    subprocess.Popen(["python3", "cameralisten.py"])
+def start_piclisten():
+    listenserver = subprocess.Popen(["python3", "cameralisten.py"])
+    return listenserver
 
-def startstreamserver():
-    subprocess.Popen(["python3", "cameraserver.py"])
+def start_streamserver():
+    streamserver = subprocess.Popen(["python3", "cameraserver.py"])
+    return streamserver
+
+def start_servicediscovery_server():
+    sdserver = subprocess.Popen(["python3", "servicediscovery_server.py"])
+    return sdserver
+
+def start_servicediscovery_client():
+    sdclient = subprocess.Popen(["python3", "servicediscovery_client.py"])
+    return sdclient
+
+listenserver = start_piclisten()
+streamserver = start_streamserver()
+sdserver = start_servicediscovery_server()
+sdclient = start_servicediscovery_client()
