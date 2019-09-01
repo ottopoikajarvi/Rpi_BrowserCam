e0bff58e73f87a8c26d5db31d390669ad6a75db4
Otto Poikajärvi <tootsi666@gmail.com>
2019-09-02 00:52:40 +0300

    download and zip work

--- Rpi_BrowserCam/multidload.py
+++ Rpi_BrowserCam/multidload.py
@@ -6,9 +6,17 @@ import cgi
 import socket
 import pickle
 import os
+import sys
+import shutil
 
 LOCAL_PORT = 5134
 TOKEN_LOC = "interrail"
+
+if len(sys.argv) != 2:
+    print("Incorrect arguments")
+    sys.exit(1)
+
+imgname = sys.argv[1]
 
 def get_devices():
     intersock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -17,9 +25,18 @@ def get_devices():
     devicesfound = pickle.loads(devicesser)
     return devicesfound
 
-imgdirectory = os.listdir("/var/www/html")
+imgdirectory = "/var/www/html/" + imgname
+os.mkdir(imgdirectory)
+devicesfound = get_devices()
 
+#retrieve images
+for device in devicesfound:
+    deviceid = device.split(".")[3]
+    imageurl = "http://192.168.1.%s/%s.jpg" % (deviceid, imgname)
+    jpgname = deviceid + ".jpg"
+    jpgloc = imgdirectory + jpgname
+    req.urlretrieve(imageurl, jpgloc)
 
-imageurl = "http://192.168.1.114/20190822163825.jpg"
-
-req.urlretrieve(imageurl, "image_name.jpg")
+#zip the folder
+zipfolder = "/var/www/html/" + imgname
+shutil.make_archive(zipfolder, "zip", imgdirectory)
